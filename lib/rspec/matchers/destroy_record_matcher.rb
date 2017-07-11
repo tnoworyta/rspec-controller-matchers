fada91b260051fad7443c9c3274e424beea7d70c
tnoworyta <tomasz.noworyta@gmail.com>
2017-07-11 11:59:49 +0200

    Specs added

--- lib/rspec/matchers/destroy_record_matcher.rb
+++ lib/rspec/matchers/destroy_record_matcher.rb
@@ -2,19 +2,40 @@ RSpec::Matchers.define :destroy_record do |record|
   supports_block_expectations
 
   match do |actual|
+    @failure_message = nil
+
     if using_service?
-      allow(service_klass).to receive(:call).and_call_original
+      allow(service_klass).to receive(:call).and_wrap_original do |original_method, *args|
+        begin
+          expect{ original_method.call(*args) }.to change { record.class.exists?(id: record.id) }.to(false)
+        rescue RSpec::Expectations::ExpectationNotMetError
+          @failure_message = "Expected a record #{record.inspect} to be destroyed by #{service_klass}, but was not"
+          return false
+        end
+      end
     end
 
-    actual.call
-
-    expect(record.class.exists?(id: record.id)).to eq false
+    begin
+      actual.call
+      expect(record.class.exists?(id: record.id)).to eq false
+    rescue RSpec::Expectations::ExpectationNotMetError
+      @failure_message = "Expected a record of #{record.class.name} class to be destroyed, but was not"
+      return false
+    end
     
     if using_service?
       expect(service_klass).to have_received(:call)
     end
     
     true
+  end
+
+  chain :using do |service_klass|
+    @service_klass = service_klass
+  end
+
+  failure_message do
+    @failure_message
   end
 
   def using_service(klass)
--- spec/rspec/matchers/destroy_record_matcher_spec.rb
+++ spec/rspec/matchers/destroy_record_matcher_spec.rb
@@ -1,6 +1,86 @@
 require "spec_helper"
 require "support/controller_specs_boilerplate"
+require "rspec/matchers/fail_matchers"
+
+class User < ActiveRecord::Base
+end
+
+
 
 describe "destroy_record matcher", type: :controller do
+  context "controller destroys record" do
+    controller do
+      def destroy
+        User.find(params[:id]).destroy
+      end
+    end
 
+    it "ensures that record is destroyed" do
+      user = User.create
+
+      expect { delete :destroy, params: { id: user.id } }.
+        to destroy_record(user)
+    end
+  end
+
+  context "service object destroys record" do
+    class DestroyUser
+      def self.call(id)
+        User.find(id).destroy
+      end
+    end
+
+    controller do
+      def destroy
+        DestroyUser.call(params[:id])
+      end
+    end
+
+    it "ensures that record is destroyed by service object" do
+      user = User.create
+
+      expect { delete :destroy, params: { id: user.id } }.
+        to destroy_record(user).using(DestroyUser)
+    end
+  end
+
+  context "service object does not destroys record but controller does" do
+    class DestroyUser
+      def self.call(id)
+      end
+    end
+
+    controller do
+      def destroy
+        DestroyUser.call(params[:id])
+        User.find(params[:id]).destroy
+      end
+    end
+
+    it "ensures that record is destroyed by service object" do
+      user = User.create
+
+      expect do
+        expect { delete :destroy, params: { id: user.id } }.
+          to destroy_record(user).using(DestroyUser)
+      end.to fail_with("Expected a record of User class to be destroyed using DestroyUser service class, but was destroyed using controller")
+    end
+  end
+
+  context "controller does not destroy" do
+    controller do
+      def destroy
+      end
+    end
+
+    it "ensures that record is not destroyed" do
+      user = User.create
+
+      expect do
+        expect do
+          delete(:destroy, params: { id: user.id })
+        end.to destroy_record(user)
+      end.to fail_with("Expected a record of User class to be destroyed, but was not")
+    end
+  end
 end
